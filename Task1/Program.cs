3636e977ee164779091d89386c85796e472d32bd
Natali <mishina_nv00@mail.ru>
2022-12-11 14:05:19 +0300

    Сделали дз3

---
+++ Task1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет  12821 -> да  23432 -> да
+
+void Polindrom(int number)
+{
+int a1 = number / 10000;
+int a5 = number % 10;
+int a2 = number / 1000;
+int a22 = number % 10;
+int a4 = number / 10;
+int a44 = number % 10;
+
+if (a1 == a5 && a22 == a44)
+{
+    Console.WriteLine("Чилсло " + number + " является палиндромом");
+}
+    else
+{
+    Console.WriteLine("Чилсло " + number + " не является палиндромом");
+}
+}
+Console.WriteLine("Введите пятизначное число");
+int number = int.Parse(Console.ReadLine());
+Polindrom(number);
+
+
+
---
+++ Task2/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние 
+// между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+double FindDistance(int x1, int y1, int x2, int y2, int z1, int z2)
+{
+    double result = Math.Sqrt(Math.Pow((x2-x1),2) + Math.Pow((y2-y1),2) + Math.Pow((z2-z1),2));
+    return result;
+}
+
+int EnterData(string text)
+{
+    Console.Write(text);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int x1 = EnterData("Введите координаты x1: ");
+int y1 = EnterData("Введите координаты y1: ");
+int z1 = EnterData("Введите координаты z1: ");
+int x2 = EnterData("Введите координаты x2: ");
+int y2 = EnterData("Введите координаты y2: ");
+int z2 = EnterData("Введите координаты z2: ");
+
+double result = FindDistance(x1, y1, z1, x2, y2, z2);
+Console.WriteLine("Расстояние между данными точками равно " + result);
